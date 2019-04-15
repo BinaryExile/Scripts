e168aa1c447f43ce0ba9a9547cb1500ba1dd3be2
BinaryExile <BinaryExile@gmail.com>
2019-04-15 19:10:06 -0400

    Update Install_Tools.sh

--- Kali Setup/Install_Tools.sh
+++ Kali Setup/Install_Tools.sh
@@ -87,6 +87,9 @@ echo -e "\n\n[*] installing xdotool \n\n"
 apt-get install -y cifs-utils sshfs exif exiv2 exfat-fuse exfat-utils nfs-common 2>> /root/errorlog.txt 1>> /root/log.txt
 apt-get install -y xdotool 2>> /root/errorlog.txt 1>> /root/log.txt
 echo -e "\n\n[*] installing ntlmrelay \n\n"
+mkdir -p `python -m site --user-site`
+cd `python -m site --user-site`
+wget https://raw.githubusercontent.com/worawit/MS17-010/master/mysmb.py
 mkdir -p /opt/ntlmrelayx 
 cd /opt/ntlmrelayx
 apt-get install -y libssl-dev libffi-dev python-dev 2>> /root/errorlog.txt 1>> /root/log.txt
@@ -106,13 +109,13 @@ echo -e "\n\n[*] installing linux-exploit-suggester \n\n"
 apt-get install -y linux-exploit-suggester 2>> /root/errorlog.txt 1>> /root/log.txt
 cd /opt/
 echo -e "\n\n[*] installing ncrack\n\n"
-mkdir /opt/ncrack
-cd /opt/ncrack
+cd /opt
 git clone https://github.com/nmap/ncrack 2>> /root/errorlog.txt 1>> /root/log.txt
-cd /opt/ncrack/ncrack
+cd /opt/ncrack
 chmod +x configure
 ./configure 2>> /root/errorlog.txt 1>> /root/log.txt ; make 2>> /root/errorlog.txt 1>> /root/log.txt ; make install 2>> /root/errorlog.txt 1>> /root/log.txt
 echo -e "\n\n[*] installing Empire \n\n"
+cd /opt/
 git clone 'https://github.com/EmpireProject/Empire' 2>> /root/errorlog.txt 1>> /root/log.txt
 cd Empire
 ./setup/install.sh
@@ -152,6 +155,7 @@ wget https://raw.githubusercontent.com/sensepost/xrdp/master/xrdp.py 2>> \root\e
 ln -s /opt/xrdp/xrdp.py /usr/local/bin/xrdp.py
 echo -e "\n\n[*] installing brutus ftp bruteforcer \n\n"
 mkdir /opt/brutus
+cd /opt/brutus
 wget https://gist.githubusercontent.com/BushiSecurity/934c2576e7dc6c0885a7f4eb2e1043b5/raw/80557a02addea2aaa9ffff64c0d80d24ceafe37b/brutus.py 2>> \root\errorlog.txt
 echo [*] updating searchsploit
 echo -e "\n\n[*] cleaning up \n\n"
@@ -164,15 +168,18 @@ echo -e "\n\n[*] Installing Firefox Developer Edition - Launch with firefox-dev 
 tar xf firefox.tar.bz2
 mv firefox/ /usr/lib/firefox-dev
 ln -s /usr/lib/firefox-dev/firefox /usr/bin/firefox-dev
-wget https://raw.githubusercontent.com/BinaryExile/Scripts/master/Kali%20Setup/firefox-dev.desktop
-mv Firefox-Dev.Desktop /usr/share/applications/Firefox-Dev.Desktop
+https://raw.githubusercontent.com/BinaryExile/Scripts/master/Kali%20Setup/Firefox-Dev.Desktop
+mv Firefox-Dev.Desktop /usr/share/applications/firefox-dev.desktop
 wget https://raw.githubusercontent.com/BinaryExile/Scripts/master/Kali%20Setup/screenshot.desktop
+mkdir -p /root/.config/autostart/
 mv screenshot.desktop  /root/.config/autostart/startup.desktop
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get update
-sudo apt --fix-broken install
-sudo apt-get install sublime-text
+apt-get update
+apt --fix-broken install
+apt-get install sublime-text
+wget -O code.deb  https://go.microsoft.com/fwlink/?LinkID=760868
+apt install ./code.deb
 cd /root/BinaryExileWiki/BinaryExile.github.io
 bundle exec jekyll serve &
 cd ~
